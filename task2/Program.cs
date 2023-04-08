9f578cd89748b802d354d5a095ef5d893ff71a57
Denis <den.dvornikov.88@inbox.ru>
2023-04-08 11:56:05 +0600

    Выполнил задание 2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int F(int m, int n)
+{
+    if (m == n)
+        return m;
+    return F(m, n - 1) + n;
+}
+
+
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(F(m, n));
