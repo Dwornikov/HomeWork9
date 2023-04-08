413066eb36a5d09f4b561d7a624223a30a72f555
Denis <den.dvornikov.88@inbox.ru>
2023-04-08 11:59:13 +0600

    Выполнил задание 3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿int A(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(A(m, n));
